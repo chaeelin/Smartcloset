0548f8c290d5f5e29cc85f01de69bb1d41eba781
hyeonjune <annhj980@naver.com>
2024-08-04 20:47:03 +0900

    cors policy

--- smartcloset/src/main/java/com/example/smartcloset/controller/ChatGptController.java
+++ smartcloset/src/main/java/com/example/smartcloset/controller/ChatGptController.java
@@ -4,6 +4,8 @@ import com.example.smartcloset.dto.ChatGptRequest;
 import com.example.smartcloset.dto.ChatGptResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
